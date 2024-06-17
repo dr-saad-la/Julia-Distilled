c75d6c3186f1544739e97af70875ef149ba4992f
dr-saad-la <dr.saad.laouadi@gmail.com>
2024-06-17 23:55:24 +0100

    Add code directory for julia codes

---
+++ code/hello.jl
@@ -0,0 +1,16 @@
+# Hello world program by Julia
+# Copyright to Dr. Saad Laouadi
+
+# Print "Hello, World!" to the console
+println("Hello, World!")
+
+# You can play a little with this function
+println("I am julia programming language!")
+println("Julia is amazing language...!")
+
+# if you are using a text editor, you can run this
+# from the command line like this:
+# julia hello.jl  
+
+# This should give you some output
+
